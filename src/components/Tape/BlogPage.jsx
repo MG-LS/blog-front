e06c9988cda3a5a0944d292a834c689642611286
MG-LS <gggggg1.97@mail.ru>
2022-05-20 16:58:14 +0300

    likelength

--- src/components/Tape/BlogPage.jsx
+++ src/components/Tape/BlogPage.jsx
@@ -12,6 +12,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { Link, useParams } from "react-router-dom";
 import "./tape.css";
 import { fetchUsers } from "../../redux/fearutes/user";
+import Footer from "../components/Footer/Footer";
 
 const BlogPage = () => {
   const blog = useSelector((state) => state.blogReducer.blog);
@@ -57,76 +58,80 @@ const BlogPage = () => {
     return "loadinggg...";
   }
   return (
-    <div className="tape_main">
-      {blog.map((item, i) => {
-        const likeFind =
-          item.likes.length && item.likes.find((i) => i === idLocal);
+    <>
+      <div className="tape_main">
+        {blog.map((item, i) => {
+          const likeFind =
+            item.likes.length && item.likes.find((i) => i === idLocal);
 
-        return (
-          <>
-            <div className="tape_map_main">
-              <div className="tape_blog">
-                {users.map((user) => {
-                  if (user._id === item.user) {
-                    return (
-                      <div className="tape_header">
-                        <div>
+          return (
+            <>
+              <div className="tape_map_main">
+                <div className="tape_blog">
+                  {users.map((user) => {
+                    if (user._id === item.user) {
+                      return (
+                        <div className="tape_header">
+                          <div>
+                            <img
+                              className=""
+                              src={`http://localhost:8000/${user.img}`}
+                            />
+                          </div>
+                          <p>{user.nickname}</p>
+                        </div>
+                      );
+                    }
+                  })}
+
+                  <div className="tape_info">
+                    <Link to={`/post/${item._id}`} className="link">
+                      <div className="tape_info_main">
+                        <div className="tape_text_info">
+                          <div>
+                            <p className="tape_title">{item.title}</p>
+                          </div>
+                          <div className="tape_text">
+                            <p>{item.text.substr(0, 100) + "..."}</p>{" "}
+                          </div>
+                        </div>
+                        <div key={i}>
                           <img
-                            className=""
-                            src={`http://localhost:8000/${user.img}`}
+                            className="tape_img"
+                            src={`http://localhost:8000/${item.img}`}
+                            alt="photo"
                           />
                         </div>
-                        <p>{user.nickname}</p>
                       </div>
-                    );
-                  }
-                })}
+                    </Link>
+                  </div>
 
-                <div className="tape_info">
-                  <Link to={`/post/${item._id}`} className="link">
-                    <div className="tape_info_main">
-                      <div className="tape_text_info">
-                        <div>
-                          <p className="tape_title">{item.title}</p>
-                        </div>
-                        <div className="tape_text">
-                          <p>{item.text.substr(0, 100) + "..."}</p>{" "}
-                        </div>
-                      </div>
-                      <div key={i}>
-                        <img
-                          className="tape_img"
-                          src={`http://localhost:8000/${item.img}`}
-                          alt="photo"
-                        />
-                      </div>
-                    </div>
-                  </Link>
-                </div>
-                <div className="tape_footer">
-                  <img src={imgMark} alt="" />
-                  {likeFind ? (
-                    <img
-                      className="tape_like"
-                      src={likeFind ? imgLikeRed : imgLike}
-                      alt=""
-                      onClick={() => deleteLikeHandle(item._id)}
-                    />
-                  ) : (
-                    <img
-                      className="tape_like"
-                      src={likeFind ? imgLikeRed : imgLike}
-                      alt=""
-                      onClick={() => addLikeHandle(item._id)}
-                    />
-                  )}
+                  <div className="tape_footer">
+                    <p>Like {item.likes.length}</p>
+
+                    {likeFind ? (
+                      <img
+                        className="tape_like"
+                        src={likeFind ? imgLikeRed : imgLike}
+                        alt=""
+                        onClick={() => deleteLikeHandle(item._id)}
+                      />
+                    ) : (
+                      <img
+                        className="tape_like"
+                        src={likeFind ? imgLikeRed : imgLike}
+                        alt=""
+                        onClick={() => addLikeHandle(item._id)}
+                      />
+                    )}
+                  </div>
                 </div>
               </div>
-            </div>
-          </>
-        );
-      })}
-    </div>
+            </>
+          );
+        })}
+      </div>
+    </>
   );
 };
 
